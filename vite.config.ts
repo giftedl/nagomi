99f7141a5778b6058fd62bf9e0f8f031dbab0242
giftedl <52332868+giftedl@users.noreply.github.com>
2026-06-14 15:18:19 +0700

    Output to `docs` since that's a valid GH Pages directory

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default {
         rolldownOptions: {
             input: getHtmlEntries(),
             output: {
-                dir: "dist"
+                dir: "docs"
             },
         }
     }
